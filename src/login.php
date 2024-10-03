11e9714f28e654e495d2b034b3eb5284fffe8b64
whowasfra <keccokecco8@gmail.com>
2024-10-04 00:48:40 +0200

    Rethinked login and registration in one page. Added sanification and hashing.

--- src/login.php
+++ src/login.php
@@ -2,16 +2,61 @@
     $user = $_POST["user"];
     $password = $_POST["password"];
 
-    function paginaConMessaggio($messaggio){
-        $r = "<!DOCTYPE html><html><head><title>Lista</title><link rel='stylesheet' href='style.css'></head><body>";
-        $r = $messaggio;
-        $r = "</p></body></html>";
-        return $r;
-    }
-
+    // Controllo che i campi non siano vuoti
     if(empty($user) || empty($password)){
-        echo paginaConMessaggio ("Inserire tutti i valori necessari");
-        die(1);
+        echo ("<scrip>alert('Errore: i campi non possono essere vuoti');
+            window.history.back();
+        </script>");
+        exit();
     }
 
+    // Connessione al database
+    $db_connection = mysqli_connect('localhost', 'root', '', 'Users');
+    if( mysqli_connect_errno() ) {
+        exit('Connessione a database non riuscita. (' . mysqli_connect_error() . ')');
+    }
     
+    // Devo registrare l'utente
+    if(isset($_POST["register"])){
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $query = "INSERT INTO Users (Username, Password, Registration_Date) VALUES (?,?, NOW())";
+        $statement = mysqli_prepare($db_connection, $query);
+        mysqli_stmt_bind_param($statement, "ss", $user, $hash);
+        
+        
+        if(!mysqli_stmt_execute($statement)){
+            echo("script>alert('Registrazione fallita, utente già presente');
+                window.history.back();
+            </script>");
+            exit();
+        }
+        else{
+            echo("<script>alert('Registrazione avvenuta con successo');
+                window.history.back();
+            </script>");
+            exit();
+        }
+    }
+    // Devo loggare l'utente
+    else{
+        $query = "SELECT Password FROM Users WHERE Username = ?";
+        $statement = mysqli_prepare($db_connection, $query);
+        mysqli_stmt_bind_param($statement, "s", $user);
+        mysqli_stmt_execute($statement);
+
+        //Binding del risultato alla variabile $hash
+        mysqli_stmt_bind_result($statement, $hash);
+        while(mysqli_stmt_fetch($statement)){
+            if(password_verify($password, $hash)){
+                echo("<script>alert('Login avvenuto con successo');
+                    window.history.back();
+                </script>");
+                exit();
+            }
+        }
+        echo("<script>alert('Login fallito');
+            window.history.back();
+        </script>");
+    }
+
+
