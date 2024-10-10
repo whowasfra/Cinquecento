dda63f509bc3738a85e546408616716e19a82aec
whowasfra <keccokecco8@gmail.com>
2024-10-10 15:36:11 +0200

    Define cards and deck

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,49 +1,17 @@
+const suits = ['coppe', 'spade', 'oro', 'bastoni'];
+const values = ['1', '2', '3', '4', '5' , '6', '7', '8', '9', '10'];
+const points = ['11','0','0','0','0','0','0','2','3','4'];
 
-const cards = [
-    {id:1b, imagePath:""},
-    {id:2b, imagePath:"images/carte/2b.bmp"},
-    {id:3b, imagePath:"images/carte/3b.bmp"},
-    {id:4b, imagePath:"images/carte/4b.bmp"},
-    {id:5b, imagePath:"images/carte/5b.bmp"},
-    {id:6b, imagePath:"images/carte/6b.bmp"},
-    {id:7b, imagePath:"images/carte/7b.bmp"},
-    {id:8b, imagePath:"images/carte/8b.bmp"},
-    {id:9b, imagePath:"images/carte/9b.bmp"},
-    {id:10b, imagePath:"images/carte/10b.bmp"},
-    {id:1c, imagePath:"images/carte/1c.bmp"},
-    {id:2c, imagePath:"images/carte/2c.bmp"},
-    {id:3c, imagePath:"images/carte/3c.bmp"},
-    {id:4c, imagePath:"images/carte/4c.bmp"},
-    {id:5c, imagePath:"images/carte/5c.bmp"},
-    {id:6c, imagePath:"images/carte/6c.bmp"},
-    {id:7c, imagePath:"images/carte/7c.bmp"},
-    {id:8c, imagePath:"images/carte/8c.bmp"},
-    {id:9c, imagePath:"images/carte/9c.bmp"},
-    {id:10c, imagePath:"images/carte/10c.bmp"},
-    {id:1o, imagePath:"images/carte/1o.bmp"},
-    {id:2o, imagePath:"images/carte/2o.bmp"},
-    {id:3o, imagePath:"images/carte/3o.bmp"},
-    {id:4o, imagePath:"images/carte/4o.bmp"},
-    {id:5o, imagePath:"images/carte/5o.bmp"},
-    {id:6o, imagePath:"images/carte/6o.bmp"},
-    {id:7o, imagePath:"images/carte/7o.bmp"},
-    {id:8o, imagePath:"images/carte/8o.bmp"},
-    {id:9o, imagePath:"images/carte/9o.bmp"},
-    {id:10o, imagePath:"images/carte/10o.bmp"},
-    {id:1s, imagePath:"images/carte/1s.bmp"},
-    {id:2s, imagePath:"images/carte/2s.bmp"},
-    {id:3s, imagePath:"images/carte/3s.bmp"},
-    {id:4s, imagePath:"images/carte/4s.bmp"},
-    {id:5s, imagePath:"images/carte/5s.bmp"},
-    {id:6s, imagePath:"images/carte/6s.bmp"},
-    {id:7s, imagePath:"images/carte/7s.bmp"},
-    {id:8s, imagePath:"images/carte/8s.bmp"},
-    {id:9s, imagePath:"images/carte/9s.bmp"},
-    {id:10s, imagePath:"images/carte/10s.bmp"},
-]
+function createDeck(){
+    let deck = [];
+    for(let suit of suits){
+        for (let value of values)
+            deck.push({suit,value});
+    }
+}
 
-
-
+let deck = createDeck();
+console.log(deck);
 
 // <div class="adversary-cards-container">
 //     <div class="adversary-card-pos-a">
