44c2197aabfc12d828f07814ac2b973b4e52524f
whowasfra <keccokecco8@gmail.com>
2024-10-15 17:05:32 +0200

    Added renderCards function, renamed images. Fixed shuffleDeck

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,38 +2,53 @@ const suits = ['coppe', 'spade', 'oro', 'bastoni'];
 const values = ['1', '2', '3', '4', '5' , '6', '7', '8', '9', '10'];
 const points = ['11','0','10','0','0','0','0','2','3','4'];
 
+function startGame(){
+    let deck = createDeck();
+    deck = shuffleDeck(deck);
+    dealHands(deck);
+    renderCards();
+}
+
 function createDeck(){
-    let deck = [];
+    deck = [];
     for(let suit of suits){
         for (let value of values)
             deck.push({suit,value});
     }
+    return deck;
 }
 
-let deck = createDeck();
-console.log(deck);
-
 function shuffleDeck(deck){
-    for(let i = deck.lenght-1; i> 0; i--){
-        const j = Math.floor(Math.random() * (i-1));
-        [deck(i),deck(j)] = [deck(j),deck(i)];
+    for(let i = deck.length-1; i> 0; i--){
+        const j = Math.floor(Math.random() * (i+1));
+        [deck[i], deck[j]] = [deck[j], deck[i]] ;
     }
     return deck;
 }
 
-let adversary_cards = [];
-let player_cards = [];
+let adversary_hand = [];
+let player_hand = [];
 
-function dealCards(deck){
-    adversary_cards = deck.splice(0,5);
-    player_cards = deck.splice(0,5);
+function dealHands(deck){
+    adversary_hand = deck.splice(0,5);
+    player_hand = deck.splice(0,5);
 }
 
-dealCards(deck);
+function renderCards(){
+    let playerCards = document.querySelector('.player-cards-container');
+    playerCards.innerHTML = '';
+    for(let i = 0; i < player_hand.length; i++){      
+        playerCards.innerHTML += `
+            <div class="player-card-pos-${i}">
+                <img src="images/carte/${player_hand[i].suit}${player_hand[i].value}.bmp" alt="${player_hand[i].suit}${player_hand[i].value}">
+            </div>
+        `;
+    }
+}
 
-console.log(adversary_cards);
-console.log(player_cards);
-console.log(deck);
+console.log(adversary_hand);
+console.log(player_hand);
+document.addEventListener("DOMContentLoaded", startGame);
 
 
 // <div class="adversary-cards-container">
