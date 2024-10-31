9c9b33e243d59488051ed81487017de928537f01
whowasfra <keccokecco8@gmail.com>
2024-10-31 20:44:59 +0100

    Add game state saving functionality and UI controls

--- src/html/game.php
+++ src/html/game.php
@@ -1,5 +1,26 @@
 <?php 
     include_once("session_config.php");
+    include_once("db_config.php");
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $userID = $_SESSION["user_id"];
+        $gameState = json_decode(file_get_contents('php://input'), true);
+        
+        $query = "INSERT INTO game_state (
+            user_id, 
+            player_points, 
+            adversary_points, 
+            player_hand, 
+            adversary_hand, 
+            player_won_cards, 
+            adversary_won_cards, 
+            briscola, 
+            briscola_declared, 
+            player_is_first, 
+            is_player_turn) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+        $statement = mysqli_prepare($db_connection, $query);
+        mysqli_stmt_bind_param($statement, "iiiiiiiiiiii", $userID, $gameState["playerPoints"], $gameState["adversaryPoints"], $gameState["playerHand"], $gameState["adversaryHand"], $gameState["playerWonCards"], $gameState["adversaryWonCards"], $gameState["briscola"], $gameState["briscolaDeclared"], $gameState["playerIsFirst"], $gameState["isPlayerTurn"]);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +54,14 @@
             </div>
         </header>
         <main>
+            <div class="menu-container">
+                <div class="menu">
+                    <button id="startGameButton" onclick="startGame()">Inizia partita</button>
+                    <button id="endGameButton" onclick="endGame()">Fine partita</button>
+                    <button id="saveGame" onclick="saveGame()"> Salva la partita</button>
+                    <button id="loadGame" onclick= "loadGame()">Carica una partita</button>
+                </div>
+            </div>
             <div class="table">
                 <canvas id = "gameCanvas" width="800" height="600" ></canvas>
             </div>    
