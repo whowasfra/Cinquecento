f0a7c2e2395bc01823f91bd92bb46f77bf322181
whowasfra <keccokecco8@gmail.com>
2024-11-07 17:49:44 +0100

    Add home.php for the main landing page with navigation and introductory sections

---
+++ src/html/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Cinquecento</title>
+        <link rel="stylesheet" href="./src/css/style.css"> 
+    </head>    
+        
+    <body>
+        <?php include '../php/topNavigation.inc.php' ?>
+        <header>
+            <div id="Cinquecento-logo">CINQUECENTO</div>
+            <a class="toggle-menu" href="#"></a>
+        </header>
+
+        <div id="content-wrap">
+            <!--Sezione introduttiva con tasti rapidi-->
+            <section id="Intro">
+                <div class="buttons">
+                    <a class="button" href="./src/html/game.php" title>Vai al gioco</a>
+                    <a class="button" href="./src/html/login.html" title>Accedi / Registrati</a>
+                    <a class="button" href="./src/html/regole.html" title>Regole</a>
+                    <a class="button" href="statistiche.html" title>Statistiche</a>
+                </div>
+            </section>
+
+            <!--Sezione storia del gioco-->
+            <section id="Storia">
+
+            </section>
+
+            <!--Sezione info progetto-->
+            <section id="Info">
+
+            </section>
+        </div>
+    </body>
+
+    <footer>
+        <div id="main-footer">
+            <div class="footer-info-wrap">
+                
+            </div>
+        </div>
+    </footer>
+</html>
