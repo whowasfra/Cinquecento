da2387811d8c849b5646da776d1339428fbf1e57
whowasfra <keccokecco8@gmail.com>
2024-11-11 18:43:45 +0100

    Add game menu styles and update navigation to use top navigation

--- html/game.php
+++ html/game.php
@@ -9,21 +9,36 @@
         <meta charset="utf-8">
         <title>Cinquecento</title>
         <link rel="stylesheet" href="../css/style.css"/>
-        <link rel="stylesheet" href="../css/leftNavigation.css"/>
+        <link rel="stylesheet" href="../css/top_navigation.css"/>
+        <link rel="stylesheet" href="../css/game.css"/>
         <script src="../scripts/deck.js"></script>
         <script src="../scripts/player.js"></script>
         <script src="../scripts/gameLogic.js"></script>
         <script src="../scripts/ui.js"></script>
+        <style>
+            .menu-container{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-top: 20px;
+            }
+            .menu {
+                display: flex;
+                flex-direction: row;
+                gap: 10px;
+            }
+            .table {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-top: 20px;
+            }
+        </style>
     </head>
 
     <body>
-        <?php include '../php/leftNavigation.php' ?>
+        <?php include '../php/top_navigation.php' ?>
         <header>
-            <div class="header-title-container">
-                <h1>Cinquecento</h1>
-            </div>
-
-            <div class="user-info-container">
                 <?php
                     if(isset($_SESSION["user"])){
                         echo("<p>Benvenuto, " . $_SESSION["user"] . " <a href='./logout.php'>Logout</a> </p>");
@@ -40,19 +55,19 @@
                 <div class="menu">
                     <button id="startGameButton" onclick="startGame()">Inizia partita</button>
                     <button id="endGameButton" onclick="endGame()">Fine partita</button>
-                    <button id="saveGame" onclick="saveGame()"> Salva la partita</button>
-                    <button id="loadGame" onclick= "loadGame()">Carica una partita</button>
+                    <button id="saveGame" onclick="saveGame()">Salva la partita</button>
+                    <button id="loadGame" onclick="loadGame()">Carica una partita</button>
                 </div>
             </div>
             <div class="table">
-                <canvas id = "gameCanvas" width="800" height="600" ></canvas>
+                <canvas id="gameCanvas" width="800" height="600"></canvas>
             </div>    
-                <div class="declaration-container">
-                    <div class="declaration-bastoni"></div>
-                    <div class="declaration-spade"></div>
-                    <div class="declaration-oro"></div>
-                    <div class="declaration-coppe"></div>
-                </div>
+            <div class="declaration-container">
+                <div class="declaration-bastoni"></div>
+                <div class="declaration-spade"></div>
+                <div class="declaration-oro"></div>
+                <div class="declaration-coppe"></div>
+            </div>
         </main>
         <footer>
             <div class="footer-title-container">
@@ -61,5 +76,4 @@
         </footer>
     </body>
 </html>
-            
-            
+
