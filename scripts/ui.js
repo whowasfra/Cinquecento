ddd22dca52ff7a2af382b7fc400e5d2252f1c4fb
whowasfra <keccokecco8@gmail.com>
2024-11-13 17:46:02 +0100

    Add rounded corners to card images and improve drawing logic

--- scripts/ui.js
+++ scripts/ui.js
@@ -33,6 +33,28 @@ class ui{
         this.ctx.shadowOffsetY = 5;
     }
 
+    setRoundedCorners(img) {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.moveTo(10, 0);
+        ctx.lineTo(canvas.width - 10, 0);
+        ctx.quadraticCurveTo(canvas.width, 0, canvas.width, 10);
+        ctx.lineTo(canvas.width, canvas.height - 10);
+        ctx.quadraticCurveTo(canvas.width, canvas.height, canvas.width - 10, canvas.height);
+        ctx.lineTo(10, canvas.height);
+        ctx.quadraticCurveTo(0, canvas.height, 0, canvas.height - 10);
+        ctx.lineTo(0, 10);
+        ctx.quadraticCurveTo(0, 0, 10, 0);
+        ctx.closePath();
+        ctx.clip();
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        return canvas;
+    }
+
     drawHand(hand, yPosition, isPlayer) {
         this.ctx.clearRect(0, yPosition, this.canvas.width, this.cardHeight);
         for (let i = 0; i < hand.length; i++) {
@@ -40,7 +62,8 @@ class ui{
             const img = new Image();
             img.src = isPlayer ? `../images/carte/${card.suit}${card.value}.bmp` : `../images/carte/dorso.bmp`;
             img.onload = () => {
-                this.ctx.drawImage(img, 50 + i * (this.cardWidth + 5), yPosition, this.cardWidth, this.cardHeight);
+                const roundedImg = this.setRoundedCorners(img);
+                this.ctx.drawImage(roundedImg, 50 + i * (this.cardWidth + 5), yPosition, this.cardWidth, this.cardHeight);
             };
         }
     }
@@ -55,34 +78,51 @@ class ui{
 
     // disegna il mazzo
     drawDeck(){
+        if (this.game.deck.cards.length === 0) {
+            this.clearDeck();
+            return;
+        }
         const img = new Image();
         img.src = `../images/carte/dorso.bmp`;
         img.onload = () => {
-            this.ctx.drawImage(img, 0,this.canvas.height/2 - this.cardHeight/2, this.cardWidth, this.cardHeight);
+            const roundedImg = this.setRoundedCorners(img);
+            this.ctx.drawImage(roundedImg, 0,this.canvas.height/2 - this.cardHeight/2, this.cardWidth, this.cardHeight);
         };
     }
 
     // disegna le carte sul tavolo
-    drawPlayedCards(){
-        this.ctx.clearRect(450,250, this.cardWidth, this.cardHeight );
-        this.ctx.clearRect(350,250  , this.cardWidth , this.cardHeight);
-         
-        if(this.game.player.playedCard){
+    drawPlayedCards() {
+        // Clear only the areas where the played cards are drawn
+        this.ctx.clearRect(350, 250, this.cardWidth, this.cardHeight);
+        this.ctx.clearRect(450, 250, this.cardWidth, this.cardHeight);
+
+        if (this.game.player.playedCard) {
             const card = this.game.player.playedCard;
             const img = new Image();
             img.src = `../images/carte/${card.suit}${card.value}.bmp`;
             img.onload = () => {
-                this.ctx.drawImage(img, 350, 250, this.cardWidth, this.cardHeight);
+                const roundedImg = this.setRoundedCorners(img);
+                this.ctx.drawImage(roundedImg, 350, 250, this.cardWidth, this.cardHeight);
             };
         }
 
-        if(this.game.adversary.playedCard){
+        if (this.game.adversary.playedCard) {
             const card = this.game.adversary.playedCard;
             const img = new Image();
             img.src = `../images/carte/${card.suit}${card.value}.bmp`;
             img.onload = () => {
-                this.ctx.drawImage(img, 450, 250, this.cardWidth, this.cardHeight);
+                const roundedImg = this.setRoundedCorners(img);
+                this.ctx.drawImage(roundedImg, 450, 250, this.cardWidth, this.cardHeight);
             };
         }
+    }
+
+    drawTakenCard(x, y) {
+        const img = new Image();
+        img.src = `../images/carte/dorso.bmp`;
+        img.onload = () => {
+            const roundedImg = this.setRoundedCorners(img);
+            this.ctx.drawImage(roundedImg, x, y, this.cardWidth, this.cardHeight);
+        };
     }
 }
