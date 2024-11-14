c05803e4ed9d30d53a58d2c0e9848b0b12ded829
whowasfra <keccokecco8@gmail.com>
2024-11-14 16:39:37 +0100

    Rename profilo.php to profile.php and include top navigation styles

--- html/profile.php
+++ html/profile.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profilo Utente</title>
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/top_navigation.css">
 </head>
 <body>
     <?php include '../php/top_navigation.php'; ?>
